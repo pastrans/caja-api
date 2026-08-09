7426fac0b71793939da3881469d0cbbd74dea350
pastrans <pastran2526@gmail.com>
2026-08-08 20:40:25 -0600

    mend

--- src/domain/use-cases/user/create-user.use-case.ts
+++ src/domain/use-cases/user/create-user.use-case.ts
@@ -1,15 +1,30 @@
 import { CreateUserDto } from '../../dtos';
 import { UserEntity } from '../../entities';
 import { UserRepository } from '../../repositories';
+import { bcryptAdapter } from '../../../config';
 
 export interface CreateUserUseCase {
   execute(dto: CreateUserDto): Promise<UserEntity>;
 }
 
 export class CreateUser implements CreateUserUseCase {
-  constructor(private readonly repository: UserRepository) {}
+  constructor(
+    private readonly repository: UserRepository,
+    private readonly hashPassword: (password: string) => string = bcryptAdapter.hash // defalut value
+  ) {}
 
-  execute(dto: CreateUserDto): Promise<UserEntity> {
-    return this.repository.create(dto);
+  async execute(dto: CreateUserDto): Promise<UserEntity> {
+    // 1. Encriptar la contraseña del DTO
+    const hashedPassword = this.hashPassword(dto.password);
+
+    // 2. Crear un DTO o payload con la contraseña encriptada
+    const [_, dtoWithHashedPassword] = CreateUserDto.create({
+      ...dto,
+      password: hashedPassword,
+    });
+
+    // 3. Guardar en el repositorio y sanitizar la respuesta
+    const user = await this.repository.create(dtoWithHashedPassword!);
+    return user.sanitize();
   }
 }
--- src/domain/use-cases/user/update-user.use-case.ts
+++ src/domain/use-cases/user/update-user.use-case.ts
@@ -1,15 +1,32 @@
 import { UpdateUserDto } from '../../dtos';
 import { UserEntity } from '../../entities';
 import { UserRepository } from '../../repositories';
+import { bcryptAdapter } from '../../../config';
 
 export interface UpdateUserUseCase {
   execute(dto: UpdateUserDto): Promise<UserEntity>;
 }
 
 export class UpdateUser implements UpdateUserUseCase {
-  constructor(private readonly repository: UserRepository) {}
+  constructor(
+    private readonly repository: UserRepository,
+    private readonly hashPassword: (password: string) => string = bcryptAdapter.hash // defalut value
+  ) {}
 
-  execute(dto: UpdateUserDto): Promise<UserEntity> {
-    return this.repository.updateById(dto);
+  async execute(dto: UpdateUserDto): Promise<UserEntity> {
+    let password = dto.password;
+
+    // Solo encriptar si el cliente envió una contraseña para actualizar
+    if (password) {
+      password = this.hashPassword(password);
+    }
+
+    const [_, dtoToUpdate] = UpdateUserDto.create({
+      ...dto,
+      password,
+    });
+
+    const user = await this.repository.updateById(dtoToUpdate!);
+    return user.sanitize();
   }
 }
